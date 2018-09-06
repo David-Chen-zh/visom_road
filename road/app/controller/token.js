3dec4ce9bb2b337ff140a1576a44f6c1c94e3cc6
David-Chen-zh <1123023582@qq.com>
2018-09-06 11:59:05 +0800

    挂载到app上

--- road/app/controller/token.js
+++ road/app/controller/token.js
@@ -1,37 +1,11 @@
 'use strict';
 const Controller = require('egg').Controller;
-const { AuthClientTwoLegged } = require('forge-apis');
-const config = require('../../config/config.js');
-
 
 class TokenController extends Controller {
 
-  /**
- * Initializes a Forge client for 2-legged authentication.
- * @param {string[]} scopes List of resource access scopes.
- * @returns {AuthClientTwoLegged} 2-legged authentication client.
- */
-  getClient(scopes) {
-    const { client_id, client_secret } = config.credentials;
-    return new AuthClientTwoLegged(client_id, client_secret, scopes || config.scopes.internal);
-  }
-
-  async getToken(scopes, ctx) {
-    const cache = {};
-    const key = scopes.join('+');
-    if (cache[key]) {
-      return cache[key];
-    }
-    const client = await this.getClient(scopes);
-    const credentials = await client.authenticate();
-    cache[key] = credentials;
-    setTimeout(() => { delete cache[key]; }, credentials.expires_in * 1000);
-    ctx.body = credentials;
-  }
-
   async create(req, res, next) {
     try {
-      const token = await this.getPublicToken();
+      const token = await this.application.getPublicToken();
       res.json({
         access_token: token.access_token,
         expires_in: token.expires_in
@@ -39,22 +13,6 @@ class TokenController extends Controller {
     } catch (err) {
       next(err);
     }
-  }
-
-  /**
- * Retrieves a 2-legged authentication token for preconfigured public scopes.
- * @returns Token object: { "access_token": "...", "expires_at": "...", "expires_in": "...", "token_type": "..." }.
- */
-  async getPublicToken(ctx) {
-    ctx.body = await this.getToken(config.scopes.public);
-  }
-
-  /**
- * Retrieves a 2-legged authentication token for preconfigured internal scopes.
- * @returns Token object: { "access_token": "...", "expires_at": "...", "expires_in": "...", "token_type": "..." }.
- */
-  async getInternalToken(ctx) {
-    ctx.body = await this.getToken(config.scopes.internal);
   }
 
 }
